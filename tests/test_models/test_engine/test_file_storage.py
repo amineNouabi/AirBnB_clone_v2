46a985294840be8250043be5e8e0f2050fa70461
amineNouabi <aminenouabi9@gmail.com>
2024-06-29 08:39:57 +0100

    Fixed tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -26,15 +26,17 @@ class test_fileStorage(unittest.TestCase):
         """ Remove storage file at end of tests """
         try:
             os.remove('file.json')
-        except:
+        except IOError:
             pass
 
     def test_pep8_FileStorage(self):
         """Test pep8 styling."""
         style = pep8.StyleGuide(quiet=True)
         p = style.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(p.total_errors, 0, "{} pep8 Style Error{} found".format(
-            p.total_errors, 's' if p.total_errors > 1 else ''))
+        self.assertEqual(p.total_errors, 0,
+                         "{} pep8 Style Error{} found"
+                         .format(p.total_errors,
+                                 's' if p.total_errors > 1 else ''))
 
     def test_docstrings(self):
         """Check for docstrings."""
