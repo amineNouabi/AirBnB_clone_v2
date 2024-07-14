6f9bf5bcff29fc92dcf11873b28a008b0b621a09
amineNouabi <aminenouabi9@gmail.com>
2024-07-14 13:09:23 +0100

    Setup server bash script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
 #Check if system is running ubuntu
-if [ ! -d /etc/apt ]; then
+if [ ! -d "/etc/apt" ]; then
 	echo "This script is only for Ubuntu systems"
 	exit 1
 fi
 
 #Install nginx
 if ! dpkg -l | grep -q nginx; then
-	apt-get update
-	apt-get -y install nginx
+	sudo apt-get update
+	sudo apt-get -y install nginx
+
+	sudo service nginx start
+	sudo ufw allow 'Nginx HTTP'
 fi
 
 #Create directories
-mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 HTML="<!DOCTYPE html>
 <html> 
@@ -26,17 +29,17 @@ HTML="<!DOCTYPE html>
 "
 
 #Create fake html file
-echo $HTML >/data/web_static/releases/test/index.html
+echo "$HTML" >/data/web_static/releases/test/index.html
 
 #Create symbolic link
-ln -sf /data/web_static/releases/test /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 #Change ownership of /data/**/* to ubuntu
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 #Add alias to nginx configuration
 if ! grep -q "location /hbnb_static {" /etc/nginx/sites-available/default; then
-	sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+	sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 fi
 
 #Restart nginx
