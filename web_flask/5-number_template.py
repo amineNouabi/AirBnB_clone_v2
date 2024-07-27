fcd10808233afd3b80003729c0771121d29cd6c3
amineNouabi <aminenouabi9@gmail.com>
2024-07-27 21:07:58 +0100

    templating

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -43,5 +43,11 @@ def number(n):
     return '{} is a number'.format(n)
 
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """route '/number_template/<n>' returns a template with n"""
+    return render_template('5-number.html', n=n)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
