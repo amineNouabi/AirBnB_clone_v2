9a979384c85efcfb8f28a93d06c5e24dc9265543
amineNouabi <aminenouabi9@gmail.com>
2024-07-27 23:50:27 +0100

    States

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+
+Flask web app that displays a list of states
+
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def list_states():
+    """ List all states """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
